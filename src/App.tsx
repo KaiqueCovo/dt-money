c2b2081dcb2ed1e61389c2389b3e121c65433d28
Kaique Covo <kaique.kng@gmail.com>
2022-08-28 15:08:58 -0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
+import "./styles/global";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World</h1>;
+    </ThemeProvider>
+  );
+}
