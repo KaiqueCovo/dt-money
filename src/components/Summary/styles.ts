20477f8164dafdd88ea65734420015258e2fea78
Kaique Covo <kaique.kng@gmail.com>
2022-08-29 22:09:22 -0300

    feature: add summary component

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,63 @@
+import styled, { css } from "styled-components";
+
+export const SummaryContainer = styled.section`
+  position: relative;
+  top: -5rem;
+
+  width: 100%;
+  max-width: 1120px;
+
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+`;
+
+interface SummaryCardProps {
+  variant?: "green";
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+  background-color: ${(props) => props.theme["gray-600"]};
+  border-radius: 6px;
+  padding: 2rem;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: ${(props) => props.theme["gray-300"]};
+  }
+
+  strong {
+    display: block;
+    margin-top: 1rem;
+    font-size: 2rem;
+  }
+
+  ${(props) =>
+    props.variant === "green" &&
+    css`
+      background-color: ${(props) => props.theme["green-700"]};
+    `}
+`;
+
+export const NewTransactionButton = styled.button`
+  height: 50px;
+  border: 0;
+  background: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme.white};
+  font-weight: bold;
+
+  padding: 0 1.25rem;
+  border-radius: 6px;
+  cursor: pointer;
+
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: ${(props) => props.theme["green-700"]};
+  }
+`;
